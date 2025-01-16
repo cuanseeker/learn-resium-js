7f318e2e52a8b14336c9bb5e4b51ff1bd5e71eca
kiel <1kielleik1@gmail.com>
2025-01-16 17:07:34 +0700

    ok'

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,18 @@ import {
   PointGraphics,
   EntityDescription,
   ImageryLayer,
+  Model,
 } from "resium";
 import { Cartesian3, Color, UrlTemplateImageryProvider } from "cesium";
 
 import { parseXMLFileToJSONCivil3D } from "./Utilities/XmlParser";
 
-const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 1);
+const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 1000);
 const positionTwo = Cartesian3.fromDegrees(-74, 41, 1);
 
 // Define an array of geographic coordinates [longitude, latitude, height, longitude, latitude, height, ...]
 const coordinates = [
-  -75.59777, 40.03883, 1000,
+  -75.59777, 40.03883, -1,
   // Point 1
   -80.5, 35.14, 1000,
   // Point 2
@@ -27,10 +28,10 @@ const coordinates = [
 // Convert the coordinates into an array of Cartesian3 positions
 const positions = Cartesian3.fromDegreesArrayHeights(coordinates);
 
-const imageryProvider = new UrlTemplateImageryProvider({
-  url: "https://{s}.google.com/vt/lyrs=s,h,p&x={x}&y={y}&z={z}",
-  subdomains: ["mt0", "mt1", "mt2", "mt3"], // Specify subdomains
-});
+// const imageryProvider = new UrlTemplateImageryProvider({
+//   url: "https://{s}.google.com/vt/lyrs=s,h,p&x={x}&y={y}&z={z}",
+//   subdomains: ["mt0", "mt1", "mt2", "mt3"], // Specify subdomains
+// });
 
 function App() {
   // const [data, setData] = useState([]);
@@ -89,32 +90,32 @@ function App() {
   //   };
   // }, []);
 
-  const viewerRef = useRef(null);
+  // const viewerRef = useRef(null);
 
-  useEffect(() => {
-    const viewer = viewerRef.current?.cesiumElement;
+  // useEffect(() => {
+  //   const viewer = viewerRef.current?.cesiumElement;
 
-    if (viewer && viewer.baseLayerPicker) {
-      console.info(viewer);
-      console.info(viewer.baseLayerPicker);
+  //   if (viewer && viewer.baseLayerPicker) {
+  //     console.info(viewer);
+  //     console.info(viewer.baseLayerPicker);
 
-      const baseLayerPickerViewModel = viewer.baseLayerPicker.viewModel;
+  //     const baseLayerPickerViewModel = viewer.baseLayerPicker.viewModel;
 
-      // Create a custom imagery provider
-      const customImageryProvider = new UrlTemplateImageryProvider({
-        url: "https://{s}.google.com/vt/lyrs=s,h,p&x={x}&y={y}&z={z}",
-        subdomains: ["mt0", "mt1", "mt2", "mt3"],
-      });
+  //     // Create a custom imagery provider
+  //     const customImageryProvider = new UrlTemplateImageryProvider({
+  //       url: "https://{s}.google.com/vt/lyrs=s,h,p&x={x}&y={y}&z={z}",
+  //       subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  //     });
 
-      // Create a new layer entry in the baseLayerPicker
-      baseLayerPickerViewModel.imageryProviderViewModels.push({
-        name: "Custom Layer asdasdasdasdsdasdas",
-        tooltip: "Custom satellite imagery layer",
-        iconUrl: "path/to/your/icon.png", // Optional icon for display in the picker
-        creationFunction: () => customImageryProvider,
-      });
-    }
-  }, []);
+  //     // Create a new layer entry in the baseLayerPicker
+  //     baseLayerPickerViewModel.imageryProviderViewModels.push({
+  //       name: "Custom Layer asdasdasdasdsdasdas",
+  //       tooltip: "Custom satellite imagery layer",
+  //       iconUrl: "path/to/your/icon.png", // Optional icon for display in the picker
+  //       creationFunction: () => customImageryProvider,
+  //     });
+  //   }
+  // }, []);
 
   return (
     <Viewer
@@ -124,15 +125,24 @@ function App() {
       creditContainer={document.createElement("div")}
       animation={false}
       baseLayerPicker={true}
-      ref={viewerRef}
+      // ref={viewerRef}
     >
-      <ImageryLayer imageryProvider={imageryProvider} />
+      {/* <ImageryLayer imageryProvider={imageryProvider} /> */}
       <div className="p-5 rounded-xl bg-white absolute top-10 left-10 z-[1000]">
         <div>asdas</div>
         <div>asdas</div>
         <div>asdas</div>
         <div>asdas</div>
       </div>
+      <Entity>
+        <Model
+          uri="./pertagas.glb" // Path to your GLTF file
+          scale={1.0} // Scale of the model
+          minimumPixelSize={128} // Minimum size of the model in pixels
+          maximumScale={2000} // Maximum scale of the model
+          position={position}
+        />
+      </Entity>
       <Entity position={position} name="Test 1">
         <PointGraphics pixelSize={10} />
         <EntityDescription>
@@ -148,7 +158,7 @@ function App() {
           key={i}
           position={position}
           point={{ pixelSize: 10, color: Color.RED }} // Customize the point's appearance
-          description={`Point ${i + 1}`} // Optional description
+          description={`Point Test ${i + 1}`} // Optional description
         />
       ))}
       <Entity position={positionTwo} name="Test 2">
